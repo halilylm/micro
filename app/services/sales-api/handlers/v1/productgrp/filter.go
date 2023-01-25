7bc5fcf18316e51bb5167ee2028c8835132901a4
halilylm <socialthinks@halil.local>
2023-01-25 19:30:35 +0300

    implementing product handler

---
+++ app/services/sales-api/handlers/v1/productgrp/filter.go
@@ -0,0 +1,35 @@
+package productgrp
+
+import (
+	"fmt"
+	"github.com/halilylm/micro/business/core/product"
+	"net/http"
+	"strconv"
+)
+
+func getFilter(r *http.Request) (product.QueryFilter, error) {
+	values := r.URL.Query()
+
+	var filter product.QueryFilter
+	filter.ByID(values.Get("id"))
+	filter.ByName(values.Get("name"))
+
+	cost := values.Get("cost")
+	if cost != "" {
+		cst, err := strconv.ParseInt(cost, 10, 64)
+		if err != nil {
+			return product.QueryFilter{}, fmt.Errorf("invalid field filter cost format: %s", cost)
+		}
+		filter.ByCost(int(cst))
+	}
+	quantity := values.Get("quantity")
+	if quantity != "" {
+		qua, err := strconv.ParseInt(quantity, 10, 64)
+		if err != nil {
+			return product.QueryFilter{}, fmt.Errorf("invalid field filter quantity format: %s", quantity)
+		}
+		filter.ByQuantity(int(qua))
+	}
+
+	return filter, nil
+}
